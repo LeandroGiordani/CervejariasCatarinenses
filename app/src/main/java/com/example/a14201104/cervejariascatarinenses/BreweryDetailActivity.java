e33fe53a26e8538ca19d6694e951fc13e6ef0a68
Leandro Giordani <leandro.lgiordani@gmail.com>
2018-06-24 21:07:55 -0300

    Add json file for breweries list and start Brewery Detail Activity

---
+++ app/src/main/java/com/example/a14201104/cervejariascatarinenses/BreweryDetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.a14201104.cervejariascatarinenses;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BreweryDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brewery_detail);
+    }
+}
